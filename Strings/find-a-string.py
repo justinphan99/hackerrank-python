1abd5c95f4bfc360f0ea8f57b8829f1c37a25762
justinphan99 <phanquockhoa99@gmail.com>
2022-12-20 19:38:13 +0700

    strings/find-a-string

---
+++ Strings/find-a-string.py
@@ -0,0 +1,14 @@
+def count_substring(string, sub_string):
+    l = len(sub_string)
+    c = 0
+    for i in range(0,len(string)):
+        if string[i:i+l] == sub_string:
+            c += 1
+    return c
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
