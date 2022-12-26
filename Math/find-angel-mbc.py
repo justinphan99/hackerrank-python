418cdb665553b04f326d8d5f0ba16716bc486b3e
justinphan99 <phanquockhoa99@gmail.com>
2022-12-26 23:29:01 +0700

    Math/find-angel-mbc

---
+++ Math/find-angel-mbc.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+a = int(input())
+b = int(input())
+M = math.sqrt(a**2+b**2)
+print(str(round(M))+'°')
+theta = math.acos(b/M )
+print(str(round(math.degrees(theta)))+'°')
